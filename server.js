7970a02bbe8b0c53e69734fcb6fe6bb4fda9e64d
Maciej Kurek <maciejk77@gmail.com>
2016-11-26 14:22:07 +0000

    CORS issue resolved

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var data = require('./public/data/player-stats');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send(data);
 })
