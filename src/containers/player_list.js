29716e2d2a6c8d4eb4d6a80553d59d33e0047601
Maciej Kurek <maciejk77@gmail.com>
2016-12-02 01:03:35 +0000

    UI fixes, Readme updated

--- src/containers/player_list.js
+++ src/containers/player_list.js
@@ -9,23 +9,24 @@ class PlayerList extends Component {
 
   renderStats(playerStats) {
 
-    // I cannot get it to parse correctly on the app
-    // const goalsPerMatch = (parseInt(playerStats.name['goals']) / parseInt(playerStats.stats.name['appearances'])).toFixed(2);
-    // const passesPerMinute = ((parseInt(playerStats.name['fwd_pass']) + parseInt(playerStats.name['backward_pass']) / parseInt(playerStats.name['mins_played)).toFixed(2);
-
     // List of stat properties we want to show;
     // If any is found in JSON but not in this array, it'll be ignored;
     const statsToDisplay = ['goals', 'goal_assist', 'appearances'];
     const el = { 'goals': 'Goals', 'goal_assist': 'Assists', 'appearances': 'Appearances'};
 
     if (statsToDisplay.indexOf(playerStats.name) > -1) {
-      return (<div key={playerStats.name}>{el[playerStats.name]}
+      return (<div className="stats-group__item" key={playerStats.name}>{el[playerStats.name]}
         <span className="stats-group__item-data">{playerStats.value}</span></div>);
     } else {
       return null;
     }
     
+    statsElements.indexOf(playerStats.name)
+    
   }
+   
+    // const goalsPerMatch = (parseInt(playerStats.name['goals']) / parseInt(playerStats.stats.name['appearances'])).toFixed(2);
+    // const passesPerMinute = ((parseInt(playerStats.name['fwd_pass']) + parseInt(playerStats.name['backward_pass']) / parseInt(playerStats.name['mins_played)).toFixed(2);
 
   render() {
 
@@ -49,9 +50,10 @@ class PlayerList extends Component {
           <div className="card__info">{position}</div>
         </div>
         <div className="card__stats-group">
-          <div className="stats-group__item">{this.props.stats.map(this.renderStats)}</div>
-          <div className="stats-group__item">Goals per match</div>
-          <div className="stats-group__item">Passes per minute</div>
+          <div>{this.props.stats.map(this.renderStats)}
+          </div>
+          <div className="stats-group__item">Goals per match<span className="stats-group__item-data">TBC</span></div>
+          <div className="stats-group__item">Passes per minute<span className="stats-group__item-data">TBC</span></div>
         </div>
       </div>
     );
